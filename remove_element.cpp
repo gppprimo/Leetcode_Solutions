63ee5b74bc1ccaa3a50924370b6d29d8b77b1d39
Giuseppe Primo <giupeppe0db@gmail.com>
2022-08-05 11:32:18 +0200

    Create remove_element.cpp

---
+++ remove_element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        for(vector<int>::iterator it = nums.begin(); it != nums.end(); ++it){
+            if(*it == val){
+                nums.erase(it);
+                --it;
+            }
+
+        }
+        return nums.size();
+    }
+};
